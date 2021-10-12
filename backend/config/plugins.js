63d6375e63f70080fd5f63de619cc5703a59020e
fedro <fjodor.makaroff@gmail.com>
2021-10-12 16:02:37 +0300

    installed aws-s3 upload provider

--- backend/config/plugins.js
+++ backend/config/plugins.js
@@ -9,4 +9,15 @@ module.exports = ({ env }) => ({
       defaultTo: "alifko@platinn.ru",
     },
   },
+  upload: {
+    provider: "aws-s3",
+    providerOptions: {
+      accessKeyId: env("AWS_ACCESS_KEY"),
+      secretAccessKey: env("AWS_ACCESS_SECRET"),
+      region: env("AWS_REGION"),
+      params: {
+        Bucket: env("AWS_BUCKET_NAME")
+      },
+    },
+  },
 });
