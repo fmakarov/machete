1c876627c6e49479631ded0d2847d7a559c67bdb
fedro <fjodor.makaroff@gmail.com>
2021-10-12 20:41:21 +0000

    Updated server config for nginx

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,6 +1,8 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  cron: { enabled: true },
+  url: env('URL', 'http://localhost'),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '8906b3ba4a219a5e6e79bcd873b499f4'),
