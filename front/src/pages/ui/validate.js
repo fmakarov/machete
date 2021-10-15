3742f9e0a5f072d37f251e122821fa55728f2a91
fedro <fjodor.makaroff@gmail.com>
2021-10-15 16:30:51 +0300

    v3 - validator change to switch/case

--- front/src/pages/ui/validate.js
+++ front/src/pages/ui/validate.js
@@ -1,88 +1,51 @@
 export default function validate(values) {
-  const validators = {
-    email: (val) => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(val),
-    phone: (val) =>
-      /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/.test(val),
-    password: (val) => /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{6,}$/.test(val),
-
-    confirmation: (val) =>
-      /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/.test(
-        val
-      ),
-    zip: (val) => /^\d{6}(-\d{6})?$/.test(val),
-    name: (val) => val.length > 3,
-    message: (val) => val.length > 3,
-    city: (val) => val.length >= 0,
-    state: (val) => val.length !== 0,
-    street: (val) => val.length !== 0,
-  };
-
   const valid = {};
 
+  function email(val) {
+    return /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(val);
+  }
+
+  function phone(val) {
+    return /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/.test(val);
+  }
+
+  function password(val) {
+    return /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{6,}$/.test(val);
+  }
+
+  function name(val) {
+    return val.length > 3;
+  }
+
+  function zip(val) {
+    return /^\d{6}(-\d{6})?$/.test(val);
+  }
+
+  function city(val) {
+    return val.length >= 0;
+  }
+
   Object.keys(values).map((field) => {
-    const vv = validators[field];
-    if (typeof vv === "function") {
-      return valid[field] = vv(values[field]);
-    } else {
-      return valid[field];
+    const val = values[field];
+    switch (field) {
+      case "email":
+        return (valid[field] = email(val));
+      case "phone":
+        return (valid[field] = phone(val));
+      case "password":
+        return (valid[field] = password(val));
+      case "name":
+        return (valid[field] = name(val));
+      case "message":
+        return (valid[field] = name(val));
+      case "zip":
+        return (valid[field] = zip(val));
+      case "city":
+        return (valid[field] = city(val));
+      default:
+        return (valid[field] = val.length !== 0);
     }
   });
 
-  // Object.keys(values).map((field) => {
-  //   if (field === "email") {
-  //     const val = values[field];
-  //     function email(val) {
-  //       return /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(val);
-  //     }
-  //     valid[field] = email(val);
-  //   }
-  //   if (field === "phone") {
-  //     const val = values[field];
-  //     function phone(val) {
-  //       return /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/.test(val);
-  //     }
-  //     valid[field] = phone(val);
-  //   }
-  //   if (field === "password") {
-  //     const val = values[field];
-  //     function password(val) {
-  //       return /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{6,}$/.test(val);
-  //     }
-  //     valid[field] = password(val);
-  //   }
-  //   if (field === "name" || field === "message") {
-  //     const val = values[field];
-  //     function password(val) {
-  //       return val.length > 3;
-  //     }
-  //     valid[field] = password(val);
-  //   }
-  //   if (field === "zip") {
-  //     const val = values[field];
-  //     function password(val) {
-  //       return /^\d{6}(-\d{6})?$/.test(val);
-  //     }
-  //      (valid[field] = password(val));
-  //   }
-  //   if (field === "city") {
-  //     const val = values[field];
-  //     function password(val) {
-  //       return val.length >= 0;
-  //     }
-  //      (valid[field] = password(val));
-  //   }
-  //   if (field === "state" || field === "street") {
-  //     const val = values[field];
-  //     function password(val) {
-  //       return val.length !== 0;
-  //     }
-  //      (valid[field] = password(val));
-  //   }
-  // });
-
-  // console.log(valid)
-  // return (valid[field] = validators[field](values[field]));
-
-  // return (valid[field] = validators[field](values[field]));
   return valid;
 }
